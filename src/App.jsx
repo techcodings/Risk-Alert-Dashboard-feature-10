2a67fcb4824c93322161e249b66c229ff59823ac
techcodings <161327197+techcodings@users.noreply.github.com>
2025-11-11 14:46:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -88,10 +88,11 @@ export default function App() {
         ← Back to Home
       </a>
 
-    {/* <h1 className="main-title" style={{ marginTop: '60px' }}>
+   <h1 className="main-title" style={{ marginTop: '60px' }}>
   
-</h1> */}
-<h1 className="sub-title">⚠️ Risk & Alert Dashboard</h1>
+</h1>
+<h2 className="sub-title">⚠️ Risk & Alert Dashboard</h2>
+
 
 
       {/* Controls */}
